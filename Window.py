7049ab7c568083709aed28b227d8075224dedcdb
AlmightyM3 <matthewmmoulton@gmail.com>
2024-06-30 18:48:09 -0400

    Added player movement and a star background

--- Window.py
+++ Window.py
@@ -3,9 +3,11 @@ import pygame
 class Window:
     def __init__(self, name, size):
         self.size = size
+        self.DT = 1.0
+        self.name = name
         pygame.init()
         self.pgWindow = pygame.display.set_mode(size)
-        pygame.display.set_caption(name)
+        pygame.display.set_caption(f"{name} | dt:{self.DT}, fps:{1000/self.DT}")
         self.clock = pygame.time.Clock()
         self.run = True
 
@@ -15,10 +17,14 @@ class Window:
             return
         
         pygame.display.flip()
+        pygame.display.set_caption(f"{self.name} | dt:{self.DT}, fps:{(1000/self.DT):.2f}")
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 self.run = False
                 pygame.quit()
+                return
             else:
                 inputCallback(event)
+        
+        self.DT = self.clock.tick(500)
--- main.py
+++ main.py
@@ -1,13 +1,55 @@
+import math
+import os
 import pygame
+from random import randint, uniform
+from pygame import Vector2, Vector3
 from Window import Window
 
-WINDOW_SIZE = (600,600)
+WINDOW_SIZE = Vector2(1200,800)
+WINDOW_SIZE2 = WINDOW_SIZE//2
+STAR_RANGE = WINDOW_SIZE*4
+STAR_RANGE2 = WINDOW_SIZE*2
+dirPath = os.path.dirname(os.path.abspath(__file__)).lower()
+if "\\" in dirPath:
+    dirPath = dirPath.replace("\\", "/")
+
+class Player:
+    def __init__(self, startPos, size, imgPath):
+        self.pos = startPos
+        self.size = size
+        self.img = pygame.transform.scale(pygame.image.load(dirPath+imgPath), size).convert_alpha()
+
+    def render(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        angle = int(math.degrees(-math.atan2(mouse_y - WINDOW_SIZE2.y, mouse_x - WINDOW_SIZE2.x)))-90
+        rotatedImg = pygame.transform.rotate(self.img, angle)
+        window.pgWindow.blit(rotatedImg, WINDOW_SIZE2-(rotatedImg.get_rect().center))
+    
+    def update(self):
+        if pygame.mouse.get_pressed()[0]:
+            self.pos = self.pos+(pygame.mouse.get_pos()-WINDOW_SIZE2).clamp_magnitude(window.DT)
+
+class Star:
+    def __init__(self):
+        self.pos = Vector3(randint(-STAR_RANGE2.x, STAR_RANGE2.x), randint(-STAR_RANGE2.y, STAR_RANGE2.y), uniform(1,4))
+    
+    def render(self):
+        pos2D = Vector2((self.pos.x-player.pos.x+STAR_RANGE2.x)%STAR_RANGE.x-STAR_RANGE.x, (self.pos.y-player.pos.y+STAR_RANGE2.y)%STAR_RANGE.y-STAR_RANGE.y) / self.pos.z + WINDOW_SIZE
+        pygame.draw.circle(window.pgWindow, (255,255,255), pos2D, 10/self.pos.z)
 
 if __name__ == "__main__":
     def input(input):
         pass
             
     window = Window("It works?", WINDOW_SIZE)
+    player = Player(Vector2(0,0), Vector2(64,64), "/player.png")
+    stars = [Star() for i in range(300)]
 
     while window.run:
+        player.update()
+        
+        window.pgWindow.fill((0,0,0))
+        for star in stars:
+            star.render()
+        player.render()
         window.update(input)
