c69d955b743f8e46e0d5fde8c575a8437ab615ce
AlmightyM3 <matthewmmoulton@gmail.com>
2024-07-02 15:40:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from pygame import Vector2, Vector3
 from Window import Window
 import numpy as np
 import opensimplex # type: ignore
+import threading
 
-from TimeIt import TimeIt
+#from TimeIt import TimeIt
 
-NOISE_SCALE = 4
+NOISE_SCALE = 18
 WINDOW_SIZE = Vector2(1200,800)
 WINDOW_SIZE2 = WINDOW_SIZE//2
 STAR_RANGE = WINDOW_SIZE*4
@@ -32,6 +33,12 @@ class Player:
         window.pgWindow.blit(rotatedImg, WINDOW_SIZE2-(rotatedImg.get_rect().center))
     
     def update(self):
+        '''
+        Use acceleration for the spaceship movement, use wasd movement (in addition to mouse - accelerate until you get to midpoint between mouse point and original player position, and then decelerate.)
+        W and S move forwards and backwards, A and D rotate the ship. (asteroids controls)
+
+        Make it so that the spaceship size changes as it get's closer to a planet, corresponding to the scale (planet.radius);
+        '''
         if pygame.mouse.get_pressed()[0]:
             self.pos = self.pos+(pygame.mouse.get_pos()-WINDOW_SIZE2).clamp_magnitude(window.DT)
 
@@ -50,7 +57,7 @@ class Planet:
         self.primaryColor = (randint(1,255), randint(1,255), randint(1,255))
         self.secondaryColor = (randint(1,255), randint(1,255), randint(1,255))
         
-        self.radius = 100
+        self.radius = randint(80, 120)
         diameter = self.radius*2
         self.surface = pygame.Surface((diameter, diameter))
         imgArray = np.zeros((diameter, diameter, 3))
@@ -62,12 +69,13 @@ class Planet:
         noise = opensimplex.noise2array(size, size)
         #timer.stopwatch("Generate noise")
 
+        w = uniform(-0.4,0.4)
         r2 = self.radius*self.radius
         for x in range(diameter):
             for y in range(diameter):
                 d2 = (x-self.radius)*(x-self.radius) + (y-self.radius)*(y-self.radius)
                 if d2 < r2:
-                    imgArray[x,y] = (self.secondaryColor if (noise[x//2,y//2]+noise[(x+1)//2,y//2]+noise[x//2,(y+1)//2]+noise[(x+1)//2,(y+1)//2])/4>0 else self.primaryColor)
+                    imgArray[x,y] = (self.secondaryColor if (noise[x//2,y//2]+noise[(x+1)//2,y//2]+noise[x//2,(y+1)//2]+noise[(x+1)//2,(y+1)//2])/4>w else self.primaryColor)
                     imgArray[x,y] *= 1-d2/r2
         #timer.stopwatch("Color, crop to circle, and shade")
 
@@ -91,18 +99,39 @@ def tryAddPlanet():
     if minDist > 1000*1000:
         planets.append(Planet(player.pos+(-minPos+player.pos).normalize()*800))
 
+def subGenPlanets(rx, ry):
+    output = []
+    for x in rx:
+        for y in ry:
+            output.append(Planet(Vector2(x,y)))
+    return output
+    
+def genPlanets():
+    global planets
+    r = range(-2400, 2400, 400)
+    #subThread = threading.Thread(target=subGenPlanets, args=(r, r[:len(r)//2]))
+    #subThread.start()
+    #value1 = subGenPlanets(r, r[len(r)//2+1:])
+    #value2 = subThread.join()
+    #planets += value1+value2
+
+    for x in r:
+        for y in r:
+            planets.append(Planet(Vector2(x,y)))
+
 if __name__ == "__main__":
     def input(input):
         pass
-            
+
     window = Window("It works?", WINDOW_SIZE)
     player = Player(Vector2(0,0), Vector2(64,64), "/player.png")
     stars = [Star() for i in range(300)]
-    planets = [Planet(Vector2())]
+    planets = []
+    genPlanets()
 
     while window.run:
         player.update()
-        tryAddPlanet()
+        #tryAddPlanet()
         
         window.pgWindow.fill((0,0,0))
         for star in stars:
