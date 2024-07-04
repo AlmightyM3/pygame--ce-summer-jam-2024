d1bf38c349e305cb2c918b9641f83801acf98043
AlmightyM3 <matthewmmoulton@gmail.com>
2024-07-04 13:57:45 -0400

    Added player acceleration, reworked planet generation to be all at one and to use pre generated noise images

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
-import math
-import os
-import pygame
-from random import randint, uniform
+import math, os, pygame, opensimplex, numpy as np
+from random import randint, uniform, choice
 from pygame import Vector2, Vector3
 from Window import Window
-import numpy as np
-import opensimplex # type: ignore
-import threading
+from PIL import Image
 
-#from TimeIt import TimeIt
+
+from TimeIt import TimeIt
 
 NOISE_SCALE = 18
 WINDOW_SIZE = Vector2(1200,800)
 WINDOW_SIZE2 = WINDOW_SIZE//2
 STAR_RANGE = WINDOW_SIZE*4
 STAR_RANGE2 = WINDOW_SIZE*2
+MIN_RADIUS = 80
+MAX_RADIUS = 120
 
 dirPath = os.path.dirname(os.path.abspath(__file__)).lower()
 if "\\" in dirPath:
     dirPath = dirPath.replace("\\", "/")
 
 class Player:
-    def __init__(self, startPos, size, imgPath):
+    def __init__(self, startPos, size, moveingImgPath, stopedIngImgPath):
         self.pos = startPos
         self.size = size
-        self.img = pygame.transform.scale(pygame.image.load(dirPath+imgPath), size).convert_alpha()
+        self.moveingImg = pygame.transform.scale_by(pygame.image.load(dirPath+moveingImgPath), size).convert_alpha()
+        self.stopedImg = pygame.transform.scale_by(pygame.image.load(dirPath+stopedIngImgPath), size).convert_alpha()
+        self.velocity = Vector2()
 
     def render(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         angle = int(math.degrees(-math.atan2(mouse_y - WINDOW_SIZE2.y, mouse_x - WINDOW_SIZE2.x)))-90
-        rotatedImg = pygame.transform.rotate(self.img, angle)
+        rotatedImg = pygame.transform.rotate((self.moveingImg if pygame.mouse.get_pressed()[0] else self.stopedImg), angle)
         window.pgWindow.blit(rotatedImg, WINDOW_SIZE2-(rotatedImg.get_rect().center))
     
     def update(self):
-        '''
-        Use acceleration for the spaceship movement, use wasd movement (in addition to mouse - accelerate until you get to midpoint between mouse point and original player position, and then decelerate.)
-        W and S move forwards and backwards, A and D rotate the ship. (asteroids controls)
-
-        Make it so that the spaceship size changes as it get's closer to a planet, corresponding to the scale (planet.radius);
-        '''
         if pygame.mouse.get_pressed()[0]:
-            self.pos = self.pos+(pygame.mouse.get_pos()-WINDOW_SIZE2).clamp_magnitude(window.DT)
+            self.velocity = (self.velocity+(pygame.mouse.get_pos()-WINDOW_SIZE2).clamp_magnitude(0.5*window.DT)).clamp_magnitude(1.4)
+        else:
+            self.velocity *= 0.96
+        self.pos += self.velocity*window.DT
 
 class Star:
     def __init__(self):
@@ -52,21 +50,21 @@ class Star:
 
 #timer = TimeIt()
 class Planet:
-    def __init__(self, startPos):
+    def __init__(self, startPos, noise):
         self.pos = startPos
         self.primaryColor = (randint(1,255), randint(1,255), randint(1,255))
         self.secondaryColor = (randint(1,255), randint(1,255), randint(1,255))
         
-        self.radius = randint(80, 120)
+        self.radius = randint(MIN_RADIUS, MAX_RADIUS)
         diameter = self.radius*2
         self.surface = pygame.Surface((diameter, diameter))
         imgArray = np.zeros((diameter, diameter, 3))
 
         #imgArray[:,:] = self.primaryColor
         #timer.stopwatch()
-        opensimplex.seed(randint(0,1024))
-        size = np.array(range(self.radius+1))/NOISE_SCALE
-        noise = opensimplex.noise2array(size, size)
+        #opensimplex.seed(randint(0,1024))
+        #size = np.array(range(self.radius+1))/NOISE_SCALE
+        #noise = opensimplex.noise2array(size, size)
         #timer.stopwatch("Generate noise")
 
         w = uniform(-0.4,0.4)
@@ -85,53 +83,48 @@ class Planet:
     def render(self):
         #pygame.draw.circle(window.pgWindow, self.primaryColor, self.pos-player.pos+WINDOW_SIZE2, self.radius)
         window.pgWindow.blit(self.surface, self.pos-player.pos+WINDOW_SIZE2-Vector2(self.radius))
-
-# Still not what I'd like due to the lag spikes and sometimes uneven but predictable placement. I may try pre-placing a large number of planets on a grid and then shifting them slightly to make it appear random
-def tryAddPlanet():
-    minDist = 1000000000000
-    minPos = Vector2()
-    for planet in planets:
-        currentDist = (planet.pos.x-player.pos.x)*(planet.pos.x-player.pos.x) + (planet.pos.y-player.pos.y)*(planet.pos.y-player.pos.y)
-        if currentDist < minDist:
-            minDist = currentDist
-            minPos = planet.pos
-
-    if minDist > 1000*1000:
-        planets.append(Planet(player.pos+(-minPos+player.pos).normalize()*800))
-
-def subGenPlanets(rx, ry):
-    output = []
-    for x in rx:
-        for y in ry:
-            output.append(Planet(Vector2(x,y)))
-    return output
     
 def genPlanets():
     global planets
-    r = range(-2400, 2400, 400)
-    #subThread = threading.Thread(target=subGenPlanets, args=(r, r[:len(r)//2]))
-    #subThread.start()
-    #value1 = subGenPlanets(r, r[len(r)//2+1:])
-    #value2 = subThread.join()
-    #planets += value1+value2
+    r = range(-13000, 13000, 1300)
 
     for x in r:
         for y in r:
-            planets.append(Planet(Vector2(x,y)))
+            planets.append(Planet(Vector2(x+randint(-400,400),y+randint(-400,400)), choice(noiseImgs)))
+
+def genNoise(num):
+    for i in range(num):
+        opensimplex.seed(randint(0,1024))
+        size = np.array(range(MAX_RADIUS+1))/NOISE_SCALE
+        noise = opensimplex.noise2array(size, size)
+        img = Image.fromarray(np.multiply(np.add(noise,1),1/2))
+        img.save(f"{dirPath}/noise/{i}.tiff")
+        print(i)
+
 
 if __name__ == "__main__":
     def input(input):
         pass
 
     window = Window("It works?", WINDOW_SIZE)
-    player = Player(Vector2(0,0), Vector2(64,64), "/player.png")
+    window.pgWindow.blit(pygame.image.load(dirPath+"/loading.png"), (0,0))
+    window.update(input)
+    player = Player(Vector2(0,0), 1/32, "/SpaceshipOn.png", "/Spaceship.png")
     stars = [Star() for i in range(300)]
     planets = []
+    noiseImgs = os.listdir(f"{dirPath}/noise")
+    if not noiseImgs:
+        genNoise(32)
+        noiseImgs = os.listdir(f"{dirPath}/noise")
+
+    noiseImgs = [np.add(np.multiply(np.array(Image.open(f"{dirPath}/noise/{i}")), 2), -1) for i in noiseImgs]
+    timer = TimeIt()
+    timer.stopwatch()
     genPlanets()
+    timer.stopwatch("Gen planets.")
 
     while window.run:
         player.update()
-        #tryAddPlanet()
         
         window.pgWindow.fill((0,0,0))
         for star in stars:
